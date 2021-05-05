5c08e503008baeff08abf93226f33af63090cf95
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-05 04:41:43 +0200

    init app

--- server/server.js
+++ server/server.js
@@ -1,28 +1,37 @@
-const bodyParser = require('body-parser');
+//importing dependecies
 const cors = require('cors');
 const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
 
-const port = 9000;
+//port && secret
+const PORT = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
+//init express server
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+app.use(
+  cors(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false,
+  })
+);
 
+//routes
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
+  const { email, password } = req.body;
+  const user = db.users.list().find(user => user.email === email);
   if (!(user && user.password === password)) {
     res.sendStatus(401);
     return;
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
 });
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+//listening
+app.listen(PORT, () =>
+  console.info(`Server started on port ${PORT}, visit http://localhost:${PORT}`)
+);
