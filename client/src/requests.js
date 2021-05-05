fa780513a4c74334b9e70a266317dab56c0f4d6e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-06 01:50:26 +0200

    adding mutation request create job from client

--- client/src/requests.js
+++ client/src/requests.js
@@ -75,3 +75,20 @@ export const loadCompany = async id => {
   const { company } = await graphQLRequest(query, { id });
   return company;
 };
+
+export const createJob = async input => {
+  const mutation = `
+    mutation CreateJob($input: CreateJobInput) {
+      job: createJob(input: $input) {
+        id
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { job } = await graphQLRequest(mutation, { input });
+  return job;
+};
