847ba7f09be8ab18cc36c6b64f78b9b79acb5e04
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-05 21:02:17 +0200

    refactoring request functions

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,14 +1,23 @@
 const URL = 'http://localhost:9000/graphql';
 
 //requests
-export const loadJobs = async () => {
+export const graphQLRequest = async (query, variables = {}) => {
   const response = await fetch(URL, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
     },
     body: JSON.stringify({
-      query: `
+      query,
+      variables,
+    }),
+  });
+  const responseBody = await response.json();
+  return responseBody.data;
+};
+
+export const loadJobs = async () => {
+  const query = `
         {
           jobs {
             id
@@ -19,37 +28,24 @@ export const loadJobs = async () => {
             }
           }
         }
-        `,
-    }),
-  });
-  const responseBody = await response.json();
-  return responseBody.data.jobs;
+  `;
+  const { jobs } = await graphQLRequest(query);
+  return jobs;
 };
 
 export const loadJob = async id => {
-  const response = await fetch(URL, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `
-        query JobQuery($id: ID!) {
-          job(id: $id) {
-            id
-            title
-            company {
-              name
-              description
-            }
-          }
+  const query = `
+      query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          name
+          description
         }
-        `,
-      variables: {
-        id,
-      },
-    }),
-  });
-  const responseBody = await response.json();
-  return responseBody.data.job;
+      }
+    }
+  `;
+  const { job } = await graphQLRequest(query, { id });
+  return job;
 };
